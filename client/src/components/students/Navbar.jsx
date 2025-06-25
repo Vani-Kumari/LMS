496146d54e131bf143d21c64e93f5b0a6954f55e
vani-Kumari <vanikumari.225014@gmail.com>
2025-06-25 14:46:50 +0530

    Educator NavBar

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -19,20 +19,20 @@ const Navbar = () => {
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
           <div className='flex items-center gap-5'>
            {user && <> 
-              <button onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              <button className='cursor-pointer' onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
            | <Link to='/my-enrollments'>My Enrollments</Link> </>}
           </div>
           {user? <UserButton/>
-          : <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button> }
+          : <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer'>Create Account</button> }
         </div>
         <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
           <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
-            {user && <> <button>Become Educator</button>
+            {user && <> <button className='cursor-pointer'>Become Educator</button>
            | <Link to='/my-enrollments'>My Enrollments</Link> </>}
           </div>
           {
             user? <UserButton/> :
-            <button onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+            <button className='cursor-pointer' onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
           }
           
         </div>
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
 
 const Educator = () => {
   return (
-    <div>
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar/>
       <div>
         {<Outlet/>}
       </div>
